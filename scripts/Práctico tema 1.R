32f42c0ccf585556799ab5f2d489f291e9f5be9a
FaCamacho <49065868+FaCamacho@users.noreply.github.com>
2021-06-26 19:31:27 -0300

    Up

--- scripts/Práctico tema 1.R
+++ scripts/Práctico tema 1.R
@@ -1,4 +1,14 @@
+library(tidyverse)
+
 d<-mpg
+
+# ggplot (data = <DATA> ) +
+#   <GEOM_FUNCTION> (mapping = aes( <MAPPINGS> ),
+#                    stat = <STAT> , position = <POSITION> ) +
+#   <COORDINATE_FUNCTION> +
+#   <FACET_FUNCTION> +
+#   <SCALE_FUNCTION> +
+#   <THEME_FUNCTION>
 
 #Asignament 1
 
@@ -246,29 +256,189 @@ ggplot(data = mpg) +
     
     
 #Asignament 5
+  
     
-    getwd()
-    
-    #i
     #¿Qué hace el parámetro geom_col? ¿En qué se diferencia de geom_bar?
+      
+      
+      #Tanto geom_bar como geom_col realizan graficos de barra
+      #geom_col requiere que se le especifique una variable en el eje Y, pues las alturas que les asigna a las barras las obtiene de un valor dado en el conjunto de datos. A diferencia de geom_bar, que realiza la operación de contar el numero de casos en cada posición de X.
+      
+      
+      
+      # La gran mayoría de geometrías y de stats vienen por parejas que siempre se utilizan en conjunto. Por ejemplo geom_bar con stats_count. Haz una pasada por la documentación y la chuleta de ggplot y establece una relación entre esas parejas de funciones. ¿Qué tienen todas en común?
+      
+      
+      
+      #Las STATS permiten agregar capas al grafico sobre la base de nuevas variables que computa a partir del conjunto de datos, pero que no estan presentes como variables en el data frame.
+
+      #Las parejas de geometrías y stats son las siguientes
+      
+      
+      
+    #geom_area y stat_bin
     
-    #ii
-    #La gran mayoría de geometrías y de stats vienen por parejas que siempre se utilizan en conjunto.
-    #Por ejemplo geom_bar con stats_count. Haz una pasada por la documentación y la chuleta de ggplot y
-    #establece una relación entre esas parejas de funciones. ¿Qué tienen todas en común?
+    ggplot(mpg, aes(hwy))+
+      
+      geom_area(stat = "bin")
     
-    #iii
-    #Qué variables calcula la función stat_smooth? ¿Qué parámetros controlan su comportamiento?
     
-    #iv
-    #Cuando hemos pintado nuestro diagrama de barras con sus proporciones, necesitamos configurar el parámetro group = 1.
-    #¿Por qué?
     
-    #vi
+    ggplot(mpg, aes(hwy))+
+      
+      stat_bin(binwidth = 1, origin = 10)
+    
+    
+    
+    #geom_density y stat_density
+    
+    ggplot(mpg, aes(hwy))+
+      
+      geom_density(kernel = "gaussian")
+    
+    
+    
+    ggplot(mpg, aes(hwy))+
+      
+      stat_density(adjust=1,kernel = "gaussian")
+    
+    
+    
+    #Geom_density2d y stat_density_2d
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      geom_density2d()
+    
+    
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      stat_density_2d(contour = T)
+    
+    
+    
+    #Geom_bind2 y stat_bind2
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      geom_bin2d()
+    
+    
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      stat_bin_2d(bins = 30, drop = T)
+    
+    
+    
+    #Geom_contour y stat_contour
+    
+    seals$z <- with(seals, sqrt(delta_long^2
+                                
+                                + delta_lat^2))
+    
+    ggplot(seals, aes(x=long,y=lat))+
+      
+      geom_contour(aes(z=z))
+    
+    
+    
+    ggplot(seals, aes(x=long,y=lat))+
+      
+      stat_contour(aes(z=z))
+    
+    
+    
+    #Geom_boxplot Stat_boxplot
+    
+    ggplot(mpg, aes(x=class,y=hwy))+
+      
+      geom_boxplot()
+    
+    
+    
+    ggplot(mpg, aes(x=class,y=hwy))+
+      
+      stat_boxplot()
+    
+    
+    
+    #Geom_violin y stat_ydensity
+    
+    ggplot(mpg, aes(x=class,y=hwy))+
+      
+      geom_violin()
+    
+    
+    
+    ggplot(mpg, aes(x=class,y=hwy))+
+      
+      stat_ydensity()
+    
+    
+    
+    #Geom_quantil y stat_quantile
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      geom_quantile()
+    
+    
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      stat_quantile()
+    
+    
+    
+    #Geom_smooth y stat_smooth
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      geom_smooth()
+    
+    
+    
+    ggplot(mpg, aes(x=cty,y=hwy))+
+      
+      stat_smooth()
+    
+    
+    
+    #¿Qué variables calcula la función stat_smooth? ¿Qué parámetros controlan su comportamiento?
+      
+      # proyecta la linea de regresión de la función Y ~ X, mostrando los valores predichos de Y para cada X.
+      #Entre los parametros que se pueden especificar, se encuentra la "formula", pudiendo especificar relaciones no lineales entre las variables. Se puede modificar el método "method" de suavizado, la presencia o no del intervalo de confianza (con "se"),y el nivel de confianza, entre otros.
+      
+      
+      
+      #Cuando hemos pintado nuestro diagrama de barras con sus proporciones, necesitamos configurar el parámetro group = 1. ¿Por qué?
+      
+      #La especificación de dicho parametro permite que el calculo de la proporción se haga sobre la base de la cantidad de observaciones para cada valor de X sobre el total de observaciones en el conjunto de datos
+      
+      
+      
     #¿Qué problema tienen los dos siguientes gráficos?
-    
-    ggplot(data = diamonds) + 
-      geom_bar(mapping = aes(x = cut, y = ..prop..))
+      
+      ggplot(data = diamonds) + 
+      geom_bar(mapping = aes(x = cut, y = ..prop.. ))
     
     ggplot(data = diamonds) + 
       geom_bar(mapping = aes(x = cut, fill = color, y = ..prop..))
+    
+    
+    
+    
+    
+    #Los dos graficos presentan el problema de que la proporción esta calculada como el total de casos sobre el total de casos, por lo que da 100%, para todos los niveles del factor agregado al eje X. Se requiere especificar que considere como grupo cada nivel de manera que calcule el % de casos que representa respecto al total de observaciones.
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
